10b1f2d02c0b93cdf8d3ff185982adbb3f818b85
malika2470 <malika2470@gmail.com>
2025-02-16 21:41:04 -0500

    Set up backend server with Express

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+//workplace to process incoming emails 
+
+// load environment variables from  .env 
+require('dotenv').config(); 
+
+// import required modules 
+const express =require('express'); // express.js framework for building APIs
+const cors = require('cors'); // allows cross-origin requests (for Chrome extension communication)
+const bodyParser = require('body-parser'); // helps process incoming JSON data
+
+// create an express app will handle requests and responses 
+const app = express(); 
+
+app.use(cors()); // will allow the chrome extension to send requests to the backend 
+
+app.use(bodyParser.json()); // automatically will process JSON data (emails sent from the extension)
+
+const PORT = process.env.PORT || 3000; 
+
+// test route 
+
+app.get('/', (req, res) => {
+    res.send('Backend is running...')
+}); 
+
+// Start the server and listen for requests
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);  // Logs a message in the terminal when the server starts
+});
+
